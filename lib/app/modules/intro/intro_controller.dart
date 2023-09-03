70a5b7d0fa04855f9595f1de83260360461ece27
Jeongseop Yi <112690335+YiJeongseop@users.noreply.github.com>
2023-09-03 09:34:34 +0900

    feat: run image, text animation on intropage

--- lib/app/modules/intro/intro_controller.dart
+++ lib/app/modules/intro/intro_controller.dart
@@ -2,16 +2,31 @@ import 'dart:async';
 import 'package:get/get.dart';
 
 class IntroController extends GetxController {
-  var animationProgress = 0.0.obs;
+  var textProgress = 0.0.obs;
+  var imageProgress = 0.0.obs;
 
-  void startAnimation() {
+  // Finish in 1 second
+  void startTextAnimation() {
     Timer.periodic(
       const Duration(milliseconds: 10),
       (timer) {
-        if (animationProgress.value > 7) {
+        if (textProgress.value > 7) {
           timer.cancel();
         } else {
-          animationProgress.value += 0.07;
+          textProgress.value += 0.07;
+        }
+      },
+    );
+  }
+
+  void startImageAnimation() {
+    Timer.periodic(
+      const Duration(milliseconds: 10),
+      (timer) {
+        if (imageProgress.value >= 720.0) {
+          timer.cancel();
+        } else {
+          imageProgress.value += 3.6;
         }
       },
     );
--- lib/app/modules/intro/intro_page.dart
+++ lib/app/modules/intro/intro_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:manster/app/modules/intro/intro_controller.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:math' as math;
 
 class IntroPage extends StatefulWidget {
   const IntroPage({Key? key}) : super(key: key);
@@ -14,8 +15,9 @@ class _IntroPageState extends State<IntroPage> {
   final IntroController introController = Get.put(IntroController());
 
   @override
-  void initState(){
-    introController.startAnimation();
+  void initState() {
+    introController.startTextAnimation();
+    introController.startImageAnimation();
     super.initState();
   }
 
@@ -29,24 +31,40 @@ class _IntroPageState extends State<IntroPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Obx(
-                () => Text(
-                  "Manster".substring(
-                      0, (introController.animationProgress.value).toInt()),
-                  style: GoogleFonts.comingSoon(fontSize: screenSize.height * 0.06, fontWeight: FontWeight.w600),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Obx(
+                    () => Transform.rotate(
+                      angle: introController.imageProgress.value *
+                          (math.pi / 180.0), // Convert degrees to radians
+                      child: Image.asset(
+                        'assets/images/icon.png',
+                        height: screenSize.height * 0.07,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: screenSize.width * 0.005,),
+                  Obx(
+                    () => Text(
+                      "Manster".substring(
+                          0, (introController.textProgress.value).toInt()),
+                      style: GoogleFonts.comingSoon(
+                          fontSize: screenSize.height * 0.07,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
               ),
-              // Text(
-              //   "IntroPage",
-              //   style: TextStyle(fontSize: screenSize.height * 0.06),
-              // ),
               TextButton(
                 onPressed: () {
                   Get.toNamed('home');
                 },
                 child: Text(
                   "Go HomePage",
-                  style: GoogleFonts.comingSoon(fontSize: screenSize.height * 0.06),
+                  style: GoogleFonts.comingSoon(
+                      fontSize: screenSize.height * 0.06),
                 ),
               )
             ],
